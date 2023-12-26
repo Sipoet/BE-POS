f9e2f852dcfa381c841bc6f7a1ef18270b8ee47f
sipoet <slayoslay@gmail.com>
2023-12-26 11:54:35 +0800

    lock if failed login attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   enum :role, [:admin, :sales, :cashier, :marketing, :stock, :superadmin,:warehouse]
 
-  devise :database_authenticatable,  :trackable,
+  devise :database_authenticatable,  :trackable, :lockable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :email, uniqueness: true, allow_nil: true
