d457c75193388cc4f6f4d68c01bd9c48a3e8e3ed
sipoet <slayoslay@gmail.com>
2023-12-28 22:06:02 +0800

    add filter date range on sales report

--- app/services/item_sale/today_report_service.rb
+++ app/services/item_sale/today_report_service.rb
@@ -1,22 +1,34 @@
 class ItemSale::TodayReportService < BaseService
 
   def execute_service
-    key = find_key!
-    sales = Sale.where(tanggal: range_today)
-    report_result = group_with_key(sales, key)
-    data = sort_most(report_result)
+    find_key!
+    find_range
     limit = @params.fetch(:limit,10).to_i
-    render_json({data: data[0...limit],meta:{group_key: key, limit: limit}})
+    results = execute_sql(query_report(limit))
+    render_json({
+      data: results.map {|row| decorate_row(row) },
+      meta: {
+        group_key: @group_key,
+        limit: limit
+      }
+    })
   end
 
   private
+
+  def decorate_row(row)
+    Result.new(
+      quantity: row['quantity'], sales_total: row['sales_total'], discount_total: row['subtotal'] - row['sales_total'], identifier: row['identifier']
+    )
+  end
 
   def sort_most(report_result)
     report_result.sort { |rowa, rowb| rowb.sales_total <=> rowa.sales_total }
   end
 
-  def range_today
-    (Time.now.utc.beginning_of_day)..(Time.now.utc.end_of_day)
+  def find_range
+    @start_time = @params.fetch(:start_time,Time.now.utc.beginning_of_day).try(:to_time)
+    @end_time = @params.fetch(:end_time,Time.now.utc.end_of_day).try(:to_time)
   end
 
   def group_with_key(sales, key)
@@ -34,9 +46,9 @@ class ItemSale::TodayReportService < BaseService
   end
 
   def find_key!
-    group_key = @params[:group_key].to_s.try(:downcase)
-    return group_key if %w{brand supplier item_type}.include?(group_key)
-    raise 'group key invalid'
+    @group_key = @params[:group_key].to_s.try(:downcase)
+    raise 'group key invalid' unless %w{brand supplier item_type}.include?(@group_key)
+
   end
 
   def key_item_of(key)
@@ -45,6 +57,24 @@ class ItemSale::TodayReportService < BaseService
     when :supplier then :supplier1
     when :item_type then :jenis
     end
+  end
+
+  def query_report(limit)
+    <<~SQL
+    SELECT
+      #{Item.table_name}.#{key_item_of(@group_key)} AS identifier,
+      COALESCE(SUM(#{ItemSale.table_name}.harga * #{ItemSale.table_name}.jumlah),0) AS subtotal,
+      SUM(#{ItemSale.table_name}.jumlah) AS quantity,
+      SUM(#{ItemSale.table_name}.total) AS sales_total
+    FROM #{ItemSale.table_name}
+    INNER JOIN #{Item.table_name} on #{Item.table_name}.kodeitem = #{ItemSale.table_name}.kodeitem
+    INNER JOIN #{Sale.table_name} on #{Sale.table_name}.notransaksi = #{ItemSale.table_name}.notransaksi
+    WHERE #{Sale.table_name}.tanggal between '#{@start_time}' and '#{@end_time}'
+    GROUP BY
+      #{Item.table_name}.#{key_item_of(@group_key)}
+    ORDER BY sales_total DESC
+    limit #{limit}
+    SQL
   end
 
   class Result
--- app/services/sale/today_report_service.rb
+++ app/services/sale/today_report_service.rb
@@ -9,17 +9,23 @@ class Sale::TodayReportService < BaseService
 
   def query_report
     <<~SQL
-    select sum(totalakhir) AS sales_total,
-    count(*) as num_of_transaction,
-    sum(jmldebit) AS debit_total,
-    sum(jmlkk) AS credit_total,
-    sum(case when jmltunai > 0 then totalakhir else 0 end) AS cash_total,
-    sum(case when byr_emoney_prod = 'QRIS' then jmlemoney else 0 end) AS qris_total,
-    sum(case when byr_emoney_prod = 'online transfer' then jmlemoney else 0 end) AS online_total
-    from #{Sale.table_name} where tanggal between '#{Time.now.utc.beginning_of_day}' and '#{Time.now.utc.end_of_day}'
+    select coalesce(sum(totalakhir),0) AS sales_total,
+    coalesce(sum(subtotal) - sum(totalakhir),0) AS discount_total,
+    coalesce(count(*),0) as num_of_transaction,
+    coalesce(sum(jmldebit),0) AS debit_total,
+    coalesce(sum(jmlkk),0) AS credit_total,
+    coalesce(sum(case when jmltunai > 0 then totalakhir else 0 end),0) AS cash_total,
+    coalesce(sum(case when byr_emoney_prod = 'QRIS' then jmlemoney else 0 end),0) AS qris_total,
+    coalesce(sum(case when byr_emoney_prod = 'online transfer' then jmlemoney else 0 end),0) AS online_total
+    from #{Sale.table_name} where tanggal between '#{start_time}' and '#{end_time}'
     SQL
   end
-  def today_range
-    (Time.now.utc.beginning_of_day)..(Time.now.end_of_day)
+
+  def start_time
+    @start_time ||= @params.fetch(:start_time,Time.now.utc.beginning_of_day).try(:to_time)
+  end
+
+  def end_time
+    @end_time ||= @params.fetch(:end_time,Time.now.utc.end_of_day).try(:to_time)
   end
 end
