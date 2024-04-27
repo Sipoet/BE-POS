36a884fc66552a285134818554a8cb14ebe0f4eb
sipoet <slayoslay@gmail.com>
2024-04-27 21:57:35 +0800

    fix bug overtime calculator

--- lib/payroll/formula/overtime_hour_calculator.rb
+++ lib/payroll/formula/overtime_hour_calculator.rb
@@ -1,13 +1,14 @@
 class Payroll::Formula::OvertimeHourCalculator < Payroll::Formula::ApplicationCalculator
 
   def calculate
-    max_hour_per_day = payroll_line.variable3.presence || 1
+    max_hour_per_day = payroll_line.variable4.presence || 1
     max_hour_per_day = 1 if max_hour_per_day == 0
     @total_overtime = 0
     attendance_summary.details.each do|detail|
       offset = detail.is_late ? payroll_line.variable2  : payroll_line.variable2
-      if detail.work_hours > offset
+      if detail.work_hours >= offset + payroll_line.variable3
         @total_overtime += [(detail.work_hours - offset),max_hour_per_day].min
+        Rails.logger.info "work_hours #{detail.work_hours} offset : #{offset}, total overtime #{@total_overtime} max hour: #{max_hour_per_day}"
       end
     end
     attendance_summary.overtime_hours = @total_overtime
