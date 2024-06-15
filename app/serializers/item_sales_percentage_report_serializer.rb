743722f06ef7a4f0aa2562ad7220e2b59439d46c
sipoet <slayoslay@gmail.com>
2024-06-15 16:00:23 +0800

    fix bug gross profit

--- app/serializers/item_sales_percentage_report_serializer.rb
+++ app/serializers/item_sales_percentage_report_serializer.rb
@@ -1,8 +1,8 @@
 class ItemSalesPercentageReportSerializer
   include JSONAPI::Serializer
   attributes :item_code, :item_name, :item_type_name, :supplier_code,
-             :supplier_name, :brand_name, :percentage_sales, :item_type_desc
-
+             :supplier_name, :brand_name, :percentage_sales, :item_type_desc,
+             :gross_profit
   attribute :brand do |obj|
     obj.brand_name
   end
