e27255a6eabebb2e03b032f0809eea92a04e8948
sipoet <slayoslay@gmail.com>
2024-09-03 18:08:20 +0800

    authorize for user profile, change password min char

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authorize_user!
-
+  before_action :authorize_user!, only: [:index,:create,:destroy]
+  before_action :authorize_if_current_user!, only: [:show,:update]
   def index
     run_service_default
   end
 
   def show
+    if params[:username] == current_user.username
+      authenticate_user!
+    else
+      authorize_user!
+    end
     run_service_default
   end
 
@@ -14,10 +19,21 @@ class UsersController < ApplicationController
   end
 
   def update
+
     run_service_default
   end
 
   def destroy
     run_service_default
+  end
+
+  private
+
+  def authorize_if_current_user!
+    if params[:username] == current_user.username
+      authenticate_user!
+    else
+      authorize_user!
+    end
   end
 end
--- app/services/user/show_service.rb
+++ app/services/user/show_service.rb
@@ -1,9 +1,30 @@
 class User::ShowService < ApplicationService
-
+  include JsonApiDeserializer
   def execute_service
-    user = User.find_by(username: @params[:username])
+    extract_params
+    user = if @params[:username] =='current_user'
+      current_user
+    else
+     User.find_by(username: @params[:username])
+    end
     raise ApplicationService::RecordNotFound.new(@params[:username],User.name) if user.nil?
-    render_json(UserSerializer.new(user))
+    options = {
+      fields: @fields,
+      params:{include: @included},
+      include: @included
+    }
+    render_json(UserSerializer.new(user, options))
   end
 
+  private
+
+  def extract_params
+    allowed_columns = User::TABLE_HEADER.map(&:name)
+    allowed_fields = [:user, :role]
+    result = dezerialize_table_params(params,
+      allowed_fields: allowed_fields,
+      allowed_columns: allowed_columns)
+    @included = result.included
+    @fields = result.fields
+  end
 end
