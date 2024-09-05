06509f6fb37cc9c8c4caec0a3bfd803a5633805f
sipoet <slayoslay@gmail.com>
2024-09-05 20:02:51 +0800

    fix bug

--- app/services/user/create_service.rb
+++ app/services/user/create_service.rb
@@ -1,15 +1,38 @@
 class User::CreateService < ApplicationService
 
   def execute_service
-    permitted_params = @params.required(:data)
-                              .required(:attributes)
-                              .permit(:role_id,:username,:email,:password,:password_confirmation)
-    user = User.new(permitted_params)
-    if user.save
-      render_json(UserSerializer.new(user),{status: :created})
+    user = User.new
+    if record_save?(user)
+      options = {
+        fields: @fields,
+        include: ['role'],
+        params:{include: ['role']}
+      }
+      render_json(UserSerializer.new(user,options),{status: :created})
     else
       render_error_record(user)
     end
+  end
+
+  def record_save?(user)
+    ApplicationRecord.transaction do
+      update_attribute(user)
+      user.save!
+    end
+    return true
+  rescue => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace
+    return false
+  end
+
+  def update_attribute(user)
+    allowed_columns = User::TABLE_HEADER.map(&:name) + [:role]
+    @fields = {user: allowed_columns}
+    permitted_params = params.required(:data)
+                              .required(:attributes)
+                              .permit(allowed_columns)
+    user.attributes = permitted_params
   end
 
 end
--- app/services/user/update_service.rb
+++ app/services/user/update_service.rb
@@ -1,16 +1,39 @@
 class User::UpdateService < ApplicationService
 
   def execute_service
-    permitted_params = @params.required(:data)
-                              .required(:attributes)
-                              .permit(:role_id,:username,:email,:password,:password_confirmation)
-    user = User.find_by(username: @params[:username])
-    raise ApplicationService::RecordNotFound.new(@params[:username],User.name) if user.nil?
-    if user.update(permitted_params)
-      render_json(UserSerializer.new(user.reload))
+    user = User.find(params[:id])
+    raise RecordNotFound.new(params[:id],User.model_name.human) if user.nil?
+    if record_save?(user)
+      options = {
+        fields: @fields,
+        include: ['role'],
+        params:{include: ['role']}
+      }
+      render_json(UserSerializer.new(user, options))
     else
       render_error_record(user)
     end
+  end
+
+  def record_save?(user)
+    ApplicationRecord.transaction do
+      edit_attribute(user)
+      user.save!
+    end
+    return true
+  rescue => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace
+    return false
+  end
+
+  def edit_attribute(user)
+    allowed_columns = User::TABLE_HEADER.map(&:name) + [:role]
+    @fields = {user: allowed_columns}
+    permitted_params = params.required(:data)
+                              .required(:attributes)
+                              .permit(allowed_columns)
+    user.attributes = permitted_params
   end
 
 end
