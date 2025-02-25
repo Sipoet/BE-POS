60485b0a3b08d069a3619ffbcd64117e75293503
sipoet <slayoslay@gmail.com>
2025-02-25 20:36:27 +0800

    sale group report add group of last purchase date

--- app/services/item_report/grouped_report_service.rb
+++ app/services/item_report/grouped_report_service.rb
@@ -1,6 +1,6 @@
 class ItemReport::GroupedReportService < ApplicationService
 
-  WHITELIST_GROUP = [:brand_name,:supplier_name,:item_type_name].freeze
+  WHITELIST_GROUP = [:brand_name,:supplier_name,:item_type_name, :last_purchase_date].freeze
 
   def execute_service
     extract_params
@@ -27,6 +27,7 @@ class ItemReport::GroupedReportService < ApplicationService
   private
 
   def extract_params
+    @table_definitions = Datatable::DefinitionExtractor.new(SalesGroupBySupplierReport)
     permitted_params = @params.permit(:report_type,suppliers:[],
                                       brands:[],item_types:[],
                                       group_names:[])
@@ -40,11 +41,11 @@ class ItemReport::GroupedReportService < ApplicationService
 
   def generate_excel(data)
     generator = ExcelGenerator.new
-    table_headers = SalesGroupBySupplierReport::TABLE_HEADER.dup
+    column_definitions = @table_definitions.column_definitions
     excluded_list = WHITELIST_GROUP.dup - @group_names
-    table_headers.reject!{|table_def| excluded_list.include?(table_def.name)}
+    column_definitions.reject!{|table_def| excluded_list.include?(table_def.name)}
     sep_index = 0
-    table_headers = table_headers.sort_by do|table_def|
+    column_definitions = column_definitions.sort_by do|table_def|
       index = @group_names.index(table_def.name)
       if index.nil?
         sep_index+=1
@@ -53,7 +54,7 @@ class ItemReport::GroupedReportService < ApplicationService
         index
       end
     end
-    generator.add_column_definitions(table_headers)
+    generator.add_column_definitions(column_definitions)
     generator.add_data(data)
     generator.add_metadata({
       suppliers: @suppliers,
@@ -61,7 +62,7 @@ class ItemReport::GroupedReportService < ApplicationService
       item_types: @item_types,
       group_names: @group_names
     })
-    generator.generate('laporan-penjualan-per-supplier')
+    generator.generate('laporan-penjualan-per-grup')
   end
 
   def decorate_result(results)
@@ -71,7 +72,7 @@ class ItemReport::GroupedReportService < ApplicationService
   def query_sql
     <<~SQL
       SELECT
-        #{@group_names.join(',')},
+        #{group_names_select},
         ROUND(SUM(number_of_purchase),0) AS number_of_purchase,
         ROUND(SUM(number_of_sales),0) AS number_of_sales,
         ROUND(SUM(sales_total),0) AS sales_total,
@@ -80,10 +81,20 @@ class ItemReport::GroupedReportService < ApplicationService
       FROM item_sales_percentage_reports
         #{filter_query}
       GROUP BY
-        #{@group_names.join(',')}
+        #{group_names_field}
       ORDER BY
         #{order_query}
     SQL
+  end
+
+  def group_names_select
+    select_query = @group_names.join(',')
+    select_query.gsub('last_purchase_date',"DATE_TRUNC('DAY',last_purchase_date) AS last_purchase_date")
+  end
+
+  def group_names_field
+    select_query = @group_names.join(',')
+    select_query.gsub('last_purchase_date',"DATE_TRUNC('DAY',last_purchase_date)")
   end
 
   def order_query
