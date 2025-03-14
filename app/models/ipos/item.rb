69a475350491a43bf98f7150308fe3ff7b05664a
sipoet <slayoslay@gmail.com>
2025-03-14 15:37:43 +0800

    fix code accidentally deleted due conflict

--- app/models/ipos/item.rb
+++ app/models/ipos/item.rb
@@ -6,7 +6,12 @@ class Ipos::Item < ApplicationRecord
   belongs_to :brand, optional: true, foreign_key: :merek, primary_key: :merek
   belongs_to :item_type, foreign_key: :jenis, primary_key: :jenis
   belongs_to :supplier, optional: true, foreign_key: :supplier1, primary_key: :kode
+
   paginates_per 20
+
+  after_update do |record|
+    Cache.delete("item-serializer:ipos/items/#{record.code}")
+  end
 
   alias_attribute :code, :kodeitem
   alias_attribute :name, :namaitem
