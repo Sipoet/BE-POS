19e1a766880133c43bff4aca662b205f2ebf2587
sipoet <slayoslay@gmail.com>
2025-07-05 15:09:44 +0800

    fixed salary based payroll config

--- lib/payroll/formula/hourly_daily_calculator.rb
+++ lib/payroll/formula/hourly_daily_calculator.rb
@@ -1,9 +1,9 @@
 class Payroll::Formula::HourlyDailyCalculator < Payroll::Formula::ApplicationCalculator
-  # variable1 = amount get per month
+  # variable1 = amount of pay
   # variable2 = how many work hour include rest hour per day.
   #             if empty or zero, how many hours based on scheduled
   # variable3 = include sick day? 1 is true, anything else is false
-
+  # variable4 = how many day get amount of pay. if empty use total day scheduled work
   def calculate
     total_work_day = 0
     min_work_calc = lambda{|detail| payroll_line.variable2.to_d}
@@ -18,12 +18,13 @@ class Payroll::Formula::HourlyDailyCalculator < Payroll::Formula::ApplicationCal
       Rails.logger.debug "#{@employee.name} #{detail.date} kerja #{work_hours} jam. min work #{min_work} total work days #{total_work_day}"
     end
     attendance_summary.total_full_work_days = total_work_day.round(1)
+    separator = (payroll_line.variable4 || attendance_summary.total_day).to_d
     fraction = if include_sick_day?(payroll_line)
-      (attendance_summary.total_full_work_days.to_d + attendance_summary.sick_leave) / attendance_summary.total_day.to_d
+      attendance_summary.total_full_work_days.to_d + attendance_summary.sick_leave
     else
-      attendance_summary.total_full_work_days.to_d / attendance_summary.total_day.to_d
+      attendance_summary.total_full_work_days.to_d
     end
-    (fraction * payroll_line.variable1.to_d).round(payslip_round)
+    (fraction * payroll_line.variable1.to_d / separator).round(payslip_round)
   end
 
   def self.main_amount(payroll_line)
