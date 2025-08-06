2a02cd623d2194b35ab64be0fe8060f51822cc54
sipoet <slayoslay@gmail.com>
2025-08-06 21:07:21 +0800

    fix bug

--- app/serializers/discount_item_type_serializer.rb
+++ app/serializers/discount_item_type_serializer.rb
@@ -2,5 +2,5 @@ class DiscountItemTypeSerializer
   include JSONAPI::Serializer
   attributes :item_type_name, :is_exclude
 
-  belongs_to :item_type, set_id: :item_type_name, id_method_name: :item_type_name
+  belongs_to :item_type, set_id: :item_type_name, id_method_name: :item_type_name,serializer: Ipos::ItemTypeSerializer
 end
