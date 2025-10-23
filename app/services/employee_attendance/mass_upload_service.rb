ed6c0345d7c0802dadb7d62b236694c09f58dc39
sipoet <slayoslay@gmail.com>
2025-10-23 14:47:30 +0800

    new template attendance due new absence machine

--- app/services/employee_attendance/mass_upload_service.rb
+++ app/services/employee_attendance/mass_upload_service.rb
@@ -42,37 +42,45 @@ class EmployeeAttendance::MassUploadService < ApplicationService
 
   def read_excel!(file)
     workbook = Xsv.open(file.path)
-    workbook['Log']
+    workbook.first
   end
 
   def extract_to_employee_attendance(sheet, range)
     first_day = range.first.try(:day)
     selected_employee = nil
     employee_attendances = []
+    row_containers = []
     sheet.to_a[4..-1].each do |rows|
-      if rows[0] == 'No :'
+      if rows[4].to_s.include?('User ID')
+        if selected_employee.present?
+          employee_attendances += extract_time_attendance(row_containers,selected_employee,range)
+          selected_employee = nil
+          row_containers = []
+        end
         selected_employee = find_employee(rows)
         next
       end
-      next if rows[0].to_s == first_day.to_s
+      next if rows[1].to_s == first_day.to_s
       next if selected_employee.nil?
-      employee_attendances += extract_time_attendance(rows,selected_employee,range)
-      selected_employee = nil
+      row_containers << rows
     end
     employee_attendances
   end
 
-  def extract_time_attendance(rows,selected_employee,range)
+  def extract_time_attendance(row_containers,selected_employee,range)
+    first_row = row_containers.first
     time_attendances = []
     finder = WorkScheduleFinder.new(selected_employee.role_id)
-    range.each.with_index do |date, index|
-      row = rows[index]
-      next if row.nil?
-      attendances = row.split("\r\n")
-                       .map{|hour| parse_time(date,hour)}
+    range.each.with_index(1) do |date, index|
+      row = first_row[index]
+      next if row.blank?
+      attendances = row_containers.map do|rows|
+        rows[index]&.split("\r\n")
+                   &.map{|hour| parse_time(date,hour)}
+      end.flatten.compact
 
-      if rows[index + 1].present?
-        next_attendances = rows[index + 1].split("\r\n")
+      if first_row[index + 1].present?
+        next_attendances = first_row[index + 1].split("\r\n")
         next_attendances.each do |hour|
           attendances << parse_time(date.tomorrow, hour)
         end
@@ -83,31 +91,16 @@ class EmployeeAttendance::MassUploadService < ApplicationService
       end_time = nil
       day_attendances = []
       attendances.each.with_index do |datetime, index|
-        if date == Date.new(2025,7,26) && selected_employee.id == 33
-          Rails.logger.debug "start_time: #{start_time} endtime: #{end_time} datetime: #{datetime}"
-        end
         if start_time.nil?
           start_time = datetime
-          if date == Date.new(2025,7,26) && selected_employee.id == 33
-          Rails.logger.debug "Masuk 0 start_time: #{start_time} endtime: #{end_time} datetime: #{datetime}"
-        end
           next
         end
         end_time = datetime
-        if date == Date.new(2025,7,26) && selected_employee.id == 33
-          Rails.logger.debug "Masuk 1 start_time: #{start_time} endtime: #{end_time} datetime: #{datetime}"
-        end
         if difference_minute(start_time,end_time) < min_read_attendance
           end_time = nil
           next
         end
-        if date == Date.new(2025,7,26) && selected_employee.id == 33
-          Rails.logger.debug "Masuk 2 start_time: #{start_time} endtime: #{end_time} datetime: #{datetime}"
-        end
         next if (attendances[index + 1].present? && difference_minute(end_time, attendances[index + 1]) < min_read_attendance)
-        if date == Date.new(2025,7,26) && selected_employee.id == 33
-          Rails.logger.debug "Masuk 3 start_time: #{start_time} endtime: #{end_time} datetime: #{datetime}"
-        end
         day_attendances << {
           employee_id: selected_employee.id,
           start_time: start_time,
@@ -167,7 +160,7 @@ class EmployeeAttendance::MassUploadService < ApplicationService
   end
 
   def find_employee(rows)
-    employee_code = rows[10].try(:downcase)
+    employee_code = rows[11].try(:downcase)
     return nil if employee_code.nil?
     Employee.find_by(code: employee_code, status: :active) || Employee.find_by(code: employee_code)
   end
@@ -181,9 +174,15 @@ class EmployeeAttendance::MassUploadService < ApplicationService
   end
 
   def extract_period(sheet)
-    period_text = sheet[2][2]
-    @start_date = Date.strptime(period_text.split('~').first.strip,'%Y/%m/%d')
-    @end_date = @start_date + (sheet[3].compact.length - 1).days
+    sheet.to_a.each.with_index(0) do |row, index|
+
+      Rails.logger.debug "index #{index} : #{row.map.with_index{|text,index2| "#{index2}: #{text}"}.join('|')}"
+    end
+    period_text = sheet[2][25]
+    period_text = period_text.to_s.split(':').last.split('~')
+    Rails.logger.debug "period text #{period_text.first} #{period_text.last}"
+    @start_date = Date.strptime(period_text.first,'%Y-%m-%d')
+    @end_date = Date.strptime(period_text.last,'%Y-%m-%d')
     @start_date..@end_date
   end
 end
