6dd23822aee608fd8d85af80898ff41178b2dfb2
sipoet <slayoslay@gmail.com>
2025-11-01 18:11:16 +0800

    rubocop fix code style

--- app/services/purchase_order/show_service.rb
+++ app/services/purchase_order/show_service.rb
@@ -16,7 +16,7 @@ class PurchaseOrder::ShowService < ApplicationService
 
   def extract_params
     @table_definitions = Datatable::DefinitionExtractor.new(Ipos::PurchaseOrder)
-    allowed_fields = [:purchase_order,:purchase_order_items,'purchase_order_items.item',:supplier]
+    allowed_fields = %i[purchase_order purchase_order_items supplier purchase_order_items.item]
     result = dezerialize_table_params(params,
                                       allowed_fields: allowed_fields,
                                       table_definitions: @table_definitions)
