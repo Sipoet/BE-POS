7909a1d671df38ae6115af7143d845a7a00e2fce
= <=>
2025-12-28 07:05:52 +0000

    fix bug

--- app/serializers/employee_leave_serializer.rb
+++ app/serializers/employee_leave_serializer.rb
@@ -3,7 +3,7 @@ class EmployeeLeaveSerializer
   attributes :leave_type, :description, :change_shift, :created_at, :updated_at
   %i[date change_date created_at updated_at].each do |key|
     attributes key do |obj|
-      obj.send(key).iso8601
+      obj.send(key)&.iso8601
     end
   end
   belongs_to :employee
