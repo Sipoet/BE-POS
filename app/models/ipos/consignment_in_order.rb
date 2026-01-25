e27fc0e07d5f819780e0e038ed60a023f0f11aff
= <=>
2026-01-25 09:55:07 +0000

    fix bug, move column order

--- app/models/ipos/consignment_in_order.rb
+++ app/models/ipos/consignment_in_order.rb
@@ -1,6 +1,6 @@
 class Ipos::ConsignmentInOrder < Ipos::OrderInHeader
   belongs_to :supplier, class_name: 'Ipos::Supplier', foreign_key: 'kodesupel', primary_key: 'kode'
-  has_one :consignment_in, class_name: 'Ipos::ConsignmentIn', foreign_key: 'notrsorder'
+  has_one :consignment_in, class_name: 'Ipos::ConsignmentIn', foreign_key: 'notrsorder', primary_key: 'notransaksi'
   def self.sti_name
     'OKI'
   end
