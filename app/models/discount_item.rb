1304c5b06557c19bd91eec345c07c2cbf2a38983
= <=>
2026-02-23 14:42:38 +0000

    fix migration

---
+++ app/models/discount_item.rb
@@ -0,0 +1,9 @@
+class DiscountItem < ApplicationRecord
+  belongs_to :item, foreign_key: :item_code, primary_key: :kodeitem, class_name: 'Ipos::Item'
+  belongs_to :discount, inverse_of: :discount_items
+
+  scope :included_items, -> { where(is_exclude: false) }
+  scope :excluded_items, -> { where(is_exclude: true) }
+
+  validates :item, presence: { message: ->(record, _data) { "item kode #{record.item_code} tidak ditemukan" } }
+end
---
+++ app/models/discount_item_type.rb
@@ -0,0 +1,8 @@
+class DiscountItemType < ApplicationRecord
+  belongs_to :item_type, foreign_key: :item_type_name, primary_key: :jenis, class_name: 'Ipos::ItemType'
+  belongs_to :discount, inverse_of: :discount_item_types
+
+  scope :included_item_types, -> { where(is_exclude: false) }
+  scope :excluded_item_types, -> { where(is_exclude: true) }
+  validates :item_type, presence: true
+end
